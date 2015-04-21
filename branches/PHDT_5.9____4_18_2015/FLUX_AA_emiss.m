3f907682a26587eba9a1def548b9de5cf27405db
huckl3b3rry87 <rescreen@live.com>
2015-04-21 18:45:22 -0400

    got ride of stuff

--- branches/PHDT_5.9____4_18_2015/FLUX_AA_emiss.m
+++
@@ -1,9 +0,0 @@
-clear;
-% TO RUN THE LATIN HYPERCUBE script %
-addpath('/home/febbo/LHC2/Optimization/Latin Hypercube/')
-
-RUN_TYPE.emiss_on = 1; % ON
-% cyc_name = 'UDDS';
-% cyc_name = 'US06';
-cyc_name = 'AA_final';
-LHC;
